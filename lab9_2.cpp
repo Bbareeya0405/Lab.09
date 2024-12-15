dab2249f1c24da68176b930433a8ffb740b598f2
BBAER <yeybreya@gmail.com>
2024-12-15 09:40:14 +0700

    Lab09.02

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N, int M) {
+    if (N <= 0 || M <= 0) {
+        std::cout << "Invalid input" << std::endl;
+        return;
+    }
+    
+    for (int i = 0; i < N; ++i) { 
+        for (int j = 0; j < M; ++j) { 
+            std::cout << "O";
+        }
+        std::cout << std::endl; 
+    }
+}
 
 int main(){
 	
